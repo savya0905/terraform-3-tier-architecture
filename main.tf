115f457a9ef3c16a1bbf0c1138645a06d1c8b2f4
savya0905 <163890691+savya0905@users.noreply.github.com>
2024-11-14 21:19:02 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -193,7 +193,7 @@ resource "aws_db_instance" "default" {
   allocated_storage      = 10
   db_subnet_group_name   = aws_db_subnet_group.default.id
   engine                 = "mysql"
-  engine_version         = "8.0.28"
+  engine_version         = "9.1.0"
   instance_class         = "db.t2.micro"
   multi_az               = false
   db_name                = "mydb"
