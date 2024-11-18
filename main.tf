685acbba37260d466a33902b040b47150cc49bdf
savya0905 <163890691+savya0905@users.noreply.github.com>
2024-11-18 16:54:11 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "ap-southeast-1a"
+  region = "ap-southeast-1"
 }
 
 # Create a VPC
@@ -25,7 +25,7 @@ resource "aws_vpc" "my-vpc" {
 resource "aws_subnet" "web-subnet-1" {
   vpc_id                  = aws_vpc.my-vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-southeast-1a"
+  availability_zone       = "ap-southeast-1"
   map_public_ip_on_launch = true
 
   tags = {
@@ -36,7 +36,7 @@ resource "aws_subnet" "web-subnet-1" {
 resource "aws_subnet" "web-subnet-2" {
   vpc_id                  = aws_vpc.my-vpc.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "ap-southeast-1a"
+  availability_zone       = "ap-southeast-1"
   map_public_ip_on_launch = true
  tags = {
     Name = "Web-2a"
@@ -49,7 +49,7 @@ resource "aws_subnet" "web-subnet-2" {
 resource "aws_subnet" "application-subnet-1" {
   vpc_id                  = aws_vpc.my-vpc.id
   cidr_block              = "10.0.11.0/24"
-  availability_zone       = "ap-southeast-1a"
+  availability_zone       = "ap-southeast-1"
   map_public_ip_on_launch = false
 
   tags = {
@@ -60,7 +60,7 @@ resource "aws_subnet" "application-subnet-1" {
 resource "aws_subnet" "application-subnet-2" {
   vpc_id                  = aws_vpc.my-vpc.id
   cidr_block              = "10.0.12.0/24"
-  availability_zone       = "ap-southeast-1a"
+  availability_zone       = "ap-southeast-1"
   map_public_ip_on_launch = false
 
   tags = {
@@ -72,7 +72,7 @@ resource "aws_subnet" "application-subnet-2" {
 resource "aws_subnet" "database-subnet-1" {
   vpc_id            = aws_vpc.my-vpc.id
   cidr_block        = "10.0.21.0/24"
-  availability_zone = "ap-southeast-1a"
+  availability_zone = "ap-southeast-1"
 
   tags = {
     Name = "Database-1a"
@@ -82,7 +82,7 @@ resource "aws_subnet" "database-subnet-1" {
 resource "aws_subnet" "database-subnet-2" {
   vpc_id            = aws_vpc.my-vpc.id
   cidr_block        = "10.0.22.0/24"
-  availability_zone = "ap-southeast-1a"
+  availability_zone = "ap-southeast-1"
 
   tags = {
     Name = "Database-2b"
@@ -128,7 +128,7 @@ resource "aws_route_table_association" "b" {
 resource "aws_instance" "webserver1" {
   ami                    = "ami-0d5eff06f840b45e9"
   instance_type          = "t2.micro"
-  availability_zone      = "ap-southeast-1a"
+  availability_zone      = "ap-southeast-1"
   key_name               = "savya"
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
   subnet_id              = aws_subnet.web-subnet-1.id
@@ -142,7 +142,7 @@ resource "aws_instance" "webserver1" {
 resource "aws_instance" "webserver2" {
   ami                    = "ami-0d5eff06f840b45e9"
   instance_type          = "t2.micro"
-  availability_zone      = "ap-southeast-1a"
+  availability_zone      = "ap-southeast-1"
   key_name               = "savya"
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
   subnet_id              = aws_subnet.web-subnet-2.id
@@ -157,7 +157,7 @@ resource "aws_instance" "webserver2" {
 resource "aws_instance" "appserver1" {
   ami                    = "ami-0d5eff06f840b45e9"
   instance_type          = "t2.micro"
-  availability_zone      = "ap-southeast-1a"
+  availability_zone      = "ap-southeast-1"
   key_name               = "savya"
   vpc_security_group_ids = [aws_security_group.appserver-sg.id]
   subnet_id              = aws_subnet.application-subnet-1.id
@@ -169,7 +169,7 @@ resource "aws_instance" "appserver1" {
 resource "aws_instance" "appserver2" {
   ami                    = "ami-0d5eff06f840b45e9"
   instance_type          = "t2.micro"
-  availability_zone      = "ap-southeast-1a"
+  availability_zone      = "ap-southeast-1"
   key_name               = "savya"
   vpc_security_group_ids = [aws_security_group.appserver-sg.id]
   subnet_id              = aws_subnet.application-subnet-2.id
